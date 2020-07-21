5ecc697463a78b4b3175f6824f61d1401135b9c4
Edouard Courty <edouard.courty2@gmail.com>
2020-07-21 18:36:30 +0200

    Initial Commit

---
+++ MenuBarPing/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MenuBarPing
+//
+//  Created by Edouard Courty on 21/07/2020.
+//  Copyright © 2020 Edouard Courty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
